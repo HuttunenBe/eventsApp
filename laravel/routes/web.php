4b216be1977327f67c28a37b4baf826f0fa4abd7
Huttunenbe <beniittahuttunen@gmail.com>
2025-06-25 09:48:43 +0300

    add laravel files

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\EventController;
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
